e4108d2fb8d30cb5cb20c5d855aa6a57cde8f000
Kartik Nayak <thisiskartiknayak@protonmail.com>
2025-04-28 19:18:07 +0530

    feat: implement dynamic status bar icon brightness based on system theme

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:dynamic_color/dynamic_color.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 import './helpers/theme_helper.dart';
 import './features/home/presentation/pages/home_page.dart';
@@ -11,6 +12,16 @@ class AppExtractor extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final brightness = MediaQuery.of(context).platformBrightness;
+
+    SystemChrome.setSystemUIOverlayStyle(
+      SystemUiOverlayStyle(
+        statusBarColor: Colors.transparent,
+        statusBarIconBrightness:
+            brightness == Brightness.dark ? Brightness.light : Brightness.dark,
+      ),
+    );
+
     return DynamicColorBuilder(
       builder: (lightDynamic, darkDynamic) {
         ColorScheme? darkColorScheme;
