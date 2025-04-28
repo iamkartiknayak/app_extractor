a0ab1ef3d3dd3efcbb94a61fe0593e03feb7997c
Kartik Nayak <thisiskartiknayak@protonmail.com>
2025-04-28 21:54:10 +0530

    feat: display relevant apps list with AppTile

--- lib/features/applist/presentation/pages/favorite_apps_page.dart
+++ lib/features/applist/presentation/pages/favorite_apps_page.dart
@@ -1,10 +1,25 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import '../widgets/app_tile.dart';
+import '../../application/applist_provider.dart';
 
 class FavoriteAppsPage extends StatelessWidget {
   const FavoriteAppsPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Center(child: Text('FavoriteAppsPage'));
+    final favoriteAppsList = context.watch<ApplistProvider>().favoriteAppsList;
+
+    return Scaffold(
+      appBar: AppBar(title: Text('Favorite Apps (${favoriteAppsList.length})')),
+      body: ListView.builder(
+        itemCount: favoriteAppsList.length,
+        itemBuilder: (context, index) {
+          final app = favoriteAppsList[index];
+          return AppTile(app: app);
+        },
+      ),
+    );
   }
 }
--- lib/features/applist/presentation/pages/installed_apps_page.dart
+++ lib/features/applist/presentation/pages/installed_apps_page.dart
@@ -1,10 +1,28 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import '../widgets/app_tile.dart';
+import '../../application/applist_provider.dart';
 
 class InstalledAppsPage extends StatelessWidget {
   const InstalledAppsPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Center(child: Text('InstalledAppsPage'));
+    final installedAppsList =
+        context.watch<ApplistProvider>().installedAppsList;
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Installed Apps (${installedAppsList.length})'),
+      ),
+      body: ListView.builder(
+        itemCount: installedAppsList.length,
+        itemBuilder: (context, index) {
+          final app = installedAppsList[index];
+          return AppTile(app: app);
+        },
+      ),
+    );
   }
 }
