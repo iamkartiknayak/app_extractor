42b945143a283899fe39451ecbf48e6862989ba6
Kartik Nayak <thisiskartiknayak@protonmail.com>
2025-04-28 19:16:39 +0530

    feat: implement dynamic color scheme to match system theme (Material)

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
+import 'package:dynamic_color/dynamic_color.dart';
 import 'package:flutter/material.dart';
 
+import './helpers/theme_helper.dart';
 import './features/home/presentation/pages/home_page.dart';
 
 void main() => runApp(AppExtractor());
@@ -9,10 +11,44 @@ class AppExtractor extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'App Extractor',
-      debugShowCheckedModeBanner: false,
-      home: HomePage(),
+    return DynamicColorBuilder(
+      builder: (lightDynamic, darkDynamic) {
+        ColorScheme? darkColorScheme;
+        ColorScheme? lightColorScheme;
+
+        if (darkDynamic != null && lightDynamic != null) {
+          darkColorScheme = darkDynamic.harmonized();
+          lightColorScheme = lightDynamic.harmonized();
+        } else {
+          darkColorScheme = ColorScheme.fromSeed(
+            seedColor: Colors.teal,
+            brightness: Brightness.dark,
+          );
+          lightColorScheme = ColorScheme.fromSeed(seedColor: Colors.teal);
+        }
+
+        return MaterialApp(
+          title: 'App Extractor',
+          debugShowCheckedModeBanner: false,
+          home: HomePage(),
+          theme: ThemeData(
+            colorScheme: lightColorScheme,
+            navigationBarTheme: NavigationBarThemeData(
+              surfaceTintColor: lightColorScheme.surfaceTint,
+            ),
+          ),
+          darkTheme: ThemeData(
+            colorScheme: darkColorScheme,
+            navigationBarTheme: NavigationBarThemeData(
+              surfaceTintColor: darkColorScheme.surfaceTint,
+            ),
+            scaffoldBackgroundColor: ThemeHelper.darkenColor(
+              darkColorScheme.surface,
+              0.03,
+            ),
+          ),
+        );
+      },
     );
   }
 }
