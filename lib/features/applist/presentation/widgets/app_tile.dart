8c947e70ad8a117ad7908844d4862535dd7cf4d0
Kartik Nayak <thisiskartiknayak@protonmail.com>
2025-04-28 23:50:39 +0530

    refactor: extract app icon and extraction logic into separate methods/providers

--- lib/features/applist/presentation/widgets/app_tile.dart
+++ lib/features/applist/presentation/widgets/app_tile.dart
@@ -1,16 +1,22 @@
 import 'package:device_apps/device_apps.dart';
 import 'package:flutter/material.dart';
 import 'package:material_symbols_icons/symbols.dart';
+import 'package:provider/provider.dart';
 
 import './fallback_icon.dart';
-import '../../../../helpers/snackbar_helper.dart';
-import '../../../../helpers/app_extract_helper.dart';
+import '../../../appinfo/application/app_info_provider.dart';
 import '../../../appinfo/presentation/pages/app_info_page.dart';
 
 class AppTile extends StatelessWidget {
   const AppTile({super.key, required this.app});
 
   final Application app;
+
+  Widget getAppIcon() {
+    return app is ApplicationWithIcon
+        ? Image.memory((app as ApplicationWithIcon).icon, width: 40, height: 40)
+        : FallbackIcon();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -19,29 +25,15 @@ class AppTile extends StatelessWidget {
           () => Navigator.of(
             context,
           ).push(MaterialPageRoute(builder: (_) => AppInfoPage(app: app))),
-      leading:
-          app is ApplicationWithIcon
-              ? Image.memory(
-                (app as ApplicationWithIcon).icon,
-                width: 40,
-                height: 40,
-              )
-              : FallbackIcon(),
+      leading: getAppIcon(),
       title: Text(app.appName),
       subtitle: Text(app.packageName),
       trailing: Row(
         mainAxisSize: MainAxisSize.min,
         children: [
           IconButton(
-            onPressed: () async {
-              final extractedPath = await AppExtractHelper.extractApk(app);
-              if (context.mounted) {
-                SnackbarHelper.showDoneExtractionSnackbar(
-                  context,
-                  extractedPath,
-                );
-              }
-            },
+            onPressed:
+                () => context.read<AppInfoProvider>().extractApk(context, app),
             icon: Icon(Symbols.unarchive),
             color: Theme.of(context).colorScheme.primary,
           ),
