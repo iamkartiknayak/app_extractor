f027064ce08ee963dd50db0e4e69b447bbeaf835
Kartik Nayak <thisiskartiknayak@protonmail.com>
2025-05-20 09:44:57 +0530

    feat: add dynamic theming with dynamic_color and system UI overlay style

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
+import 'package:dynamic_color/dynamic_color.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 import './features/home/presentation/pages/home_page.dart';
@@ -9,9 +11,51 @@ class Dexify extends StatelessWidget {
   const Dexify({super.key});
 
   @override
-  Widget build(final BuildContext context) => const MaterialApp(
-    title: 'Dexify',
-    debugShowCheckedModeBanner: false,
-    home: HomePage(),
-  );
+  Widget build(final BuildContext context) {
+    final brightness = MediaQuery.of(context).platformBrightness;
+
+    SystemChrome.setSystemUIOverlayStyle(
+      SystemUiOverlayStyle(
+        statusBarColor: Colors.transparent,
+        statusBarIconBrightness:
+            brightness == Brightness.dark ? Brightness.light : Brightness.dark,
+      ),
+    );
+
+    return DynamicColorBuilder(
+      builder: (final lightDynamic, final darkDynamic) {
+        final lightScheme =
+            lightDynamic?.harmonized() ??
+            ColorScheme.fromSeed(
+              seedColor: Colors.teal,
+              brightness: Brightness.light,
+            );
+        final darkScheme =
+            darkDynamic?.harmonized() ??
+            ColorScheme.fromSeed(
+              seedColor: Colors.teal,
+              brightness: Brightness.dark,
+            );
+
+        return MaterialApp(
+          title: 'Dexify',
+          debugShowCheckedModeBanner: false,
+          home: const HomePage(),
+          theme: ThemeData(
+            colorScheme: lightScheme,
+            navigationBarTheme: NavigationBarThemeData(
+              surfaceTintColor: lightScheme.surfaceTint,
+            ),
+          ),
+          darkTheme: ThemeData(
+            colorScheme: darkScheme,
+            navigationBarTheme: NavigationBarThemeData(
+              surfaceTintColor: darkScheme.surfaceTint,
+            ),
+            scaffoldBackgroundColor: darkScheme.surface,
+          ),
+        );
+      },
+    );
+  }
 }
